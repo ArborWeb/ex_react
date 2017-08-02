f47900bb80a6f4de02b0252b20192e2a80fa8df5
Arbor Web <guilherme@arborweb.com.br>
2017-08-02 11:52:36 -0300

    aula 28 completa

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
-import Member from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-	<Family  lastname="Weissheimer">
-		<Member name="Guilherme" position="Pai" />
-		<Member name="Rafael" position="Filho" />
-		<Member name="Julia" position="Mãe"/>
-	</Family>
+	<ClassComponent value="Componente Classe" />
 , document.getElementById('app'))
--- ex/member.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default props => (
-	<div>{props.name + ' da Silva'} <strong>{props.lastname}</strong> - {' é o/a ' + props.position}</div>
-)
